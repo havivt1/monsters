da46f280ff353b4ad66c5d5fb9e0755b4e12a854
havivt1 <havivtahory@Havivs-MacBook-Pro.local>
2020-04-14 01:25:04 +0300

    Updates

---
+++ precache-manifest.0808f24715a3a3e27eed6c7bf08ef3b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0696dfd741a1ac1720362b4c141d6321",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "5a448433cac65109735e",
+    "url": "/monsters/static/css/main.94587ddf.chunk.css"
+  },
+  {
+    "revision": "e1aa6d47886e301b7aad",
+    "url": "/monsters/static/js/2.d6f3f948.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.d6f3f948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a448433cac65109735e",
+    "url": "/monsters/static/js/main.c3c02dea.chunk.js"
+  },
+  {
+    "revision": "8ca53a20636dab62d2e4",
+    "url": "/monsters/static/js/runtime-main.2a3028ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
